11072ff44c275510baaa0759241e1e8e3983c5e7
Sebastien Castiel <sebastien@castiel.me>
2024-10-25 15:51:11 -0400

    Add tRPC, perform call to get a group

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,30 @@
 import { Stack } from 'expo-router'
 import './global.css'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import { trpc } from '@/utils/trpc'
+import superjson from 'superjson'
 
 export default function RootLayout() {
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: 'https://spliit.app/api/trpc',
+          transformer: superjson,
+        }),
+      ],
+    })
+  )
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,18 @@
+import { trpc } from '@/utils/trpc'
 import { Text, View } from 'react-native'
 
 export default function Index() {
+  const { data, isLoading, error } = trpc.groups.get.useQuery({
+    groupId: process.env.EXPO_PUBLIC_GROUP_ID ?? '',
+  })
+  console.log(data, isLoading, error)
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Edit file app/index.tsx to edit this screen.</Text>
+      <Text className="font-mono">
+        Data: {isLoading}, {JSON.stringify(data, undefined, 2)}
+      </Text>
     </View>
   )
 }
