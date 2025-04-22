944f43005ba03b143ddef947e5e5431948ddc9aa
Sebastien Castiel <sebastien@castiel.me>
2025-04-21 21:05:56 -0400

    Don't trust atoms. They make up everything!

--- app/(tabs)/[groupId]/(tabs)/balances.tsx
+++ app/(tabs)/[groupId]/(tabs)/balances.tsx
@@ -1,4 +1,5 @@
 import { TrackScreen } from '@/components/analytics'
+import { cn } from '@/utils/cn'
 import { textBrand } from '@/utils/colors'
 import { formatCurrency } from '@/utils/formatCurrency'
 import { trpc } from '@/utils/trpc'
@@ -39,22 +40,26 @@ function Balances({
   if (!data?.balances) return null
 
   const maxBalance = Math.max(
-    ...Object.values(data.balances).map(({ total }) => Math.abs(total))
+    ...Object.values(data.balances).map(({ total }) => Math.abs(total)),
   )
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['top']} className="flex-1 bg-white">
+      <SafeAreaView edges={['top']} className="flex-1 bg-background">
         {isInitialLoading ? (
           <View className="h-full flex-col justify-center items-center gap-4">
             <ActivityIndicator size="large" />
-            <Text className="text-slate-700">Loading group balances</Text>
+            <Text className="text-foreground-secondary">
+              Loading group balances
+            </Text>
           </View>
         ) : (
           <ScrollView className="flex-1">
             <View className="p-4 mt-2 mb-4">
-              <Text className="font-bold mb-0.5 text-lg">Balances</Text>
-              <Text className="text-gray-500 text-sm">
+              <Text className="font-bold mb-0.5 text-lg text-foreground">
+                Balances
+              </Text>
+              <Text className="text-foreground-secondary text-sm">
                 This is the amount that each participant paid or was paid for.
               </Text>
             </View>
@@ -72,7 +77,9 @@ function Balances({
                     (balance.total < 0 ? 'justify-start' : 'justify-end')
                   }
                 >
-                  <Text className="font-bold">{participant.name}</Text>
+                  <Text className="font-bold text-foreground">
+                    {participant.name}
+                  </Text>
                 </View>
               )
               const barElement = (
@@ -82,12 +89,12 @@ function Balances({
                       className={
                         'h-full border ' +
                         (balance.total < 0
-                          ? 'bg-red-300 border-red-400 self-end rounded-l-md'
-                          : 'bg-green-300 border-green-400 self-start rounded-r-md')
+                          ? 'bg-background-negative border-border-negative self-end rounded-l-md'
+                          : 'bg-background-positive border-border-positive self-start rounded-r-md')
                       }
                       style={{
                         width: `${Math.abs(
-                          (balance.total / maxBalance) * 100
+                          (balance.total / maxBalance) * 100,
                         )}%`,
                       }}
                     />
@@ -96,11 +103,14 @@ function Balances({
                   )}
                   <View className="px-2 absolute top-0 bottom-0 left-0 right-0 flex-col justify-center">
                     <Text
-                      className={balance.total < 0 ? 'self-end' : 'self-start'}
+                      className={cn(
+                        'text-foreground',
+                        balance.total < 0 ? 'self-end' : 'self-start',
+                      )}
                     >
                       {formatCurrency(
                         group.currency,
-                        (balance.paid || balance.paidFor) / 100
+                        (balance.paid || balance.paidFor) / 100,
                       )}
                     </Text>
                   </View>
@@ -128,10 +138,10 @@ function Balances({
             })}
 
             <View className="p-4 mt-4 mb-2">
-              <Text className="font-bold mb-0.5 text-lg">
+              <Text className="font-bold mb-0.5 text-lg text-foreground">
                 Suggested reimbursements
               </Text>
-              <Text className="text-gray-500 text-sm">
+              <Text className="text-sm text-foreground-secondary">
                 Here are suggestions for optimized reimbursements between
                 participants.
               </Text>
@@ -145,28 +155,28 @@ function Balances({
 
             {data.reimbursements.map((reimbursement, index) => {
               const participantFrom = group.participants.find(
-                (participant) => participant.id === reimbursement.from
+                (participant) => participant.id === reimbursement.from,
               )
               const participantTo = group.participants.find(
-                (participant) => participant.id === reimbursement.to
+                (participant) => participant.id === reimbursement.to,
               )
               if (!participantFrom || !participantTo) return null
 
               return (
                 <View
                   key={index}
-                  className="rounded-md mx-4 mb-2 p-4 bg-slate-100"
+                  className="rounded-md mx-4 mb-2 p-4 bg-background-card"
                 >
                   <View className="flex-row justify-between ">
-                    <Text>
+                    <Text className="text-foreground">
                       <Text className="font-bold">{participantFrom.name}</Text>
                       <Text> owes </Text>
                       <Text className="font-bold">{participantTo.name}</Text>
                     </Text>
-                    <Text className="font-bold">
+                    <Text className="font-bold text-foreground">
                       {formatCurrency(
                         group.currency,
-                        reimbursement.amount / 100
+                        reimbursement.amount / 100,
                       )}
                     </Text>
                   </View>
@@ -186,7 +196,7 @@ function Balances({
                       })
                     }}
                   >
-                    <Text className={textBrand}>Mark as paid</Text>
+                    <Text className="text-foreground-accent">Mark as paid</Text>
                   </Pressable>
                 </View>
               )
--- utils/themes.ts
+++ utils/themes.ts
@@ -12,6 +12,10 @@ export const themes = {
     '--color-background-card': colors.slate['100'],
     '--color-background-accent': 'hsl(163 94% 24%)',
     '--color-border': colors.slate['200'],
+    '--color-background-positive': colors.green['300'],
+    '--color-border-positive': colors.green['400'],
+    '--color-background-negative': colors.red['300'],
+    '--color-border-negative': colors.red['400'],
   }),
   dark: vars({
     '--color-background': colors.black,
@@ -23,5 +27,9 @@ export const themes = {
     '--color-background-card': colors.slate['900'],
     '--color-background-accent': 'hsl(161 90% 45%)',
     '--color-border': colors.slate['800'],
+    '--color-background-positive': colors.green['900'],
+    '--color-border-positive': colors.green['800'],
+    '--color-background-negative': colors.red['900'],
+    '--color-border-negative': colors.red['800'],
   }),
 }
